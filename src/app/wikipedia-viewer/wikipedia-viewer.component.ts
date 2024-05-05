3849c5223c343b663c6874068877a02e0fe76656
trhunt566 <82134266+trhunt566@users.noreply.github.com>
2024-05-04 23:53:49 -0600

    add some initial components

---
+++ src/app/wikipedia-viewer/wikipedia-viewer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wikipedia-viewer',
+  standalone: true,
+  imports: [],
+  templateUrl: './wikipedia-viewer.component.html',
+  styleUrl: './wikipedia-viewer.component.scss'
+})
+export class WikipediaViewerComponent {
+
+}
